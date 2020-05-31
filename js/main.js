98ccdcb537c1377ef90eb7ce98515c827a3e41b8
bastiman85 <github@bastiman.nu>
2020-05-31 04:58:43 +0200

    Updated js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
+// document.getElementById("hero__container").style.marginTop = "135px";
 
 navToggle.addEventListener("click", () => {
    nav.classList.toggle("nav--visible");
+   var h = document.getElementById("header").clientHeight;
+   document.getElementById("hero__container").style.marginTop = "calc(" + h + "px" + " - 30px)";
+   console.log(h);
 });
+
+document.getElementsByTagName("BODY")[0].onresize = function () {
+   marginal();
+};
+
+function marginal() {
+   var h = document.getElementById("header").clientHeight;
+   document.getElementById("hero__container").style.marginTop = "calc(" + h + "px" + " - 30px)";
+}
