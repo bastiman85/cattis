94ae30febf19e791e0c0aa9e0d8bc930c9ae517c
bastiman85 <github@bastiman.nu>
2020-07-16 20:03:28 +0200

    More prodcuts and spam protection

--- js/main.js
+++ js/main.js
@@ -16,3 +16,23 @@ function marginal() {
    var h = document.getElementById("header").clientHeight;
    document.getElementById("hero__container").style.marginTop = "calc(" + h + "px" + " - 30px)";
 }
+
+const moreButton = document.querySelector(".arrowtoggle");
+const moreButtonArrow = document.querySelector(".arrow");
+const moreProducts = document.querySelector(".moreproducts");
+
+moreButton.addEventListener("click", () => {
+   if (moreButtonArrow.classList.contains("fa-arrow-down")) {
+      moreButtonArrow.classList.remove("fa-arrow-down");
+      moreButtonArrow.classList.add("fa-arrow-up");
+      moreButton.classList.remove("more");
+      moreButton.classList.add("less");
+      moreProducts.classList.remove("hide");
+   } else {
+      moreButtonArrow.classList.add("fa-arrow-down");
+      moreButtonArrow.classList.remove("fa-arrow-up");
+      moreButton.classList.remove("less");
+      moreButton.classList.add("more");
+      moreProducts.classList.add("hide");
+   }
+});
